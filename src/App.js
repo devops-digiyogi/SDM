723da790fd776634755b3083edcf98bc265023da
shreyas m <shreyasm6543@gmail.com>
2021-01-10 23:54:19 +0530

    r

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main3 from './components/main3.js'
 import Main4 from './components/main4.js'
 import Main5 from './components/main5.js'
 import Main6 from './components/main6.js'
+import Footer from './components/footer.js'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Main4 />
         <Main5 />
         <Main6 />
+        <Footer />
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './../css/comp/footer.css';
+import './../css/body.css';
+
+export default function Main3() {
+    return (
+        <div className="foot">
+            <div className="footheader">
+                <div className="footheadbox">
+                    <div className="footheadtext1">About Us</div>
+                </div>
+            </div>
+            <div className="footbody">
+            </div>
+        </div>
+    );
+}
